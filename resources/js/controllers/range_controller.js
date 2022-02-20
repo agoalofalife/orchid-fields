f79560e1d02e1fdc45b65ed89f6db029a2c13394
Gruz <vongruz@protonmail.com>
2022-02-20 18:39:31 +0200

    Fixes not working with multiple ranges fields

--- resources/js/controllers/range_controller.js
+++ resources/js/controllers/range_controller.js
@@ -1,14 +1,16 @@
 export default class extends window.Controller {
     connect() {
         let value = this.data.get('value');
-        
-        $(this.element.querySelector(".js-range-slider")).ionRangeSlider({
+
+        let input = this.element.querySelector(".js-range-slider");
+
+        $(input).ionRangeSlider({
             onFinish: function (data) {
-                data.input[0].dispatchEvent(new Event('change'));
+                input.dispatchEvent(new Event('change'));
             },
         });
-        
-        let range = $(".js-range-slider").data("ionRangeSlider");
+
+        let range = $(input).data("ionRangeSlider");
 
         if (value !== null) {
             let [from, to] = value.split(';');
