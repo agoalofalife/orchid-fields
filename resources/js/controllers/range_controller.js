5352f94bdee520b9ec8306b6137037f4cea91a96
Gruz <vongruz@protonmail.com>
2022-02-20 16:56:39 +0200

    Makes field work with Orchid listener

--- resources/js/controllers/range_controller.js
+++ resources/js/controllers/range_controller.js
@@ -1,7 +1,13 @@
 export default class extends window.Controller {
     connect() {
         let value = this.data.get('value');
-        $(this.element.querySelector(".js-range-slider")).ionRangeSlider();
+        
+        $(this.element.querySelector(".js-range-slider")).ionRangeSlider({
+            onFinish: function (data) {
+                data.input[0].dispatchEvent(new Event('change'));
+            },
+        });
+        
         let range = $(".js-range-slider").data("ionRangeSlider");
 
         if (value !== null) {
