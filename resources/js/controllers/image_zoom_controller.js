4f23adf32c70dcfc2bb490acad302b9ad819e774
Ilya <agoalofalife@gmail.com>
2023-01-12 19:49:57 +0700

    fix: conflict with table orchid 🐛

--- resources/js/controllers/image_zoom_controller.js
+++ resources/js/controllers/image_zoom_controller.js
@@ -3,6 +3,12 @@ import Zooming from 'zooming'
 export default class extends window.Controller {
     connect() {
         const zooming = new Zooming({
+            onBeforeOpen: function () {
+                document.querySelector('.table-responsive').style.overflowX = 'visible'
+            },
+            onClose: function () {
+                document.querySelector('.table-responsive').style.overflowX = 'auto'
+            },
             customSize: this.data.get('zoompercent') || 100 + '%'
         })
 
