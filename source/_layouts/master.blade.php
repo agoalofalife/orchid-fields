3380add76dab4ad4ae815a64df2751ff23080471
agoalofalife <agoalofalife@gmail.com>
2023-01-30 15:58:50 +0700

    fix: change config for fix assets path in github pages 🐛

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -34,7 +34,7 @@
         @endif
 
         <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,300i,400,400i,700,700i,800,800i" rel="stylesheet">
-        <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
+        <link rel="stylesheet" href="{{ mix('css/main.css', "{$page->baseUrl}assets/build") }}">
 
         @if ($page->docsearchApiKey && $page->docsearchIndexName)
             <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/docsearch.js@2/dist/cdn/docsearch.min.css" />
