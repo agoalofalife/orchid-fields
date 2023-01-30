633451a4eb13ee6e08cd9a73a973bba2b4423717
agoalofalife <agoalofalife@gmail.com>
2023-01-30 13:40:53 +0700

    fix: change config for fix assets path in github pages 🐛

--- config.production.php
+++ config.production.php
@@ -3,7 +3,7 @@
 return [
     'baseUrl' => 'https://jigsaw-docs-template.tighten.co',
     'production' => true,
-
+    'subDirectory' => env('SUB_DIRECTORY', '/orchid-fields'),
     // DocSearch credentials
     'docsearchApiKey' => env('DOCSEARCH_KEY'),
     'docsearchIndexName' => env('DOCSEARCH_INDEX'),
