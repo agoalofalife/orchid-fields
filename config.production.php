c98eebc731ecf0e1a17f7e162096fce56853b76a
agoalofalife <agoalofalife@gmail.com>
2023-01-30 14:07:09 +0700

    fix: change assets path for correct image 🐛

--- config.production.php
+++ config.production.php
@@ -4,6 +4,7 @@ return [
     'baseUrl' => 'https://jigsaw-docs-template.tighten.co',
     'production' => true,
     'pathToAssets' => '/orchid-fields/assets/build',
+
     // DocSearch credentials
     'docsearchApiKey' => env('DOCSEARCH_KEY'),
     'docsearchIndexName' => env('DOCSEARCH_INDEX'),
