87469118dd2cf15d6bfb6ef953aafda5e1859535
Anatole <44148694+Abrakso@users.noreply.github.com>
2024-03-05 12:33:08 +0300

    disable table overflow on zoom image (#9)

--- resources/js/controllers/image_zoom_controller.js
+++ resources/js/controllers/image_zoom_controller.js
@@ -3,24 +3,34 @@ export default class extends window.Controller {
     connect() {
         const zooming = new Zooming({
             onBeforeOpen: function() {
-                var table = document.querySelector('.table-responsive');
+                var tableResponsive = document.querySelector('.table-responsive');
+                var table = document.querySelector('.table');
                 var form = document.querySelector('#post-form');
+                var itHasHappenedInResponsiveTable = tableResponsive !== null;
                 var itHasHappenedInTable = table !== null;
                 var itHasHappenedInPostForm = form !== null;
-                if (itHasHappenedInTable) {
+                if (itHasHappenedInResponsiveTable) {
                     document.querySelector('.table-responsive').style.overflowX = 'visible';
+                }
+                if (itHasHappenedInTable) {
+                    table.style.overflow = 'visible';
                 }
                 if (itHasHappenedInPostForm) {
                     form.classList.remove('overflow-hidden');
                 }
             },
             onClose: function() {
-                var table = document.querySelector('.table-responsive');
+                var tableResponsive = document.querySelector('.table-responsive');
+                var table = document.querySelector('.table');
                 var form = document.querySelector('#post-form');
+                var itHasHappenedInResponsiveTable = tableResponsive !== null;
                 var itHasHappenedInTable = table !== null;
                 var itHasHappenedInPostForm = form !== null;
-                if (itHasHappenedInTable) {
+                if (itHasHappenedInResponsiveTable) {
                     document.querySelector('.table-responsive').style.overflowX = 'auto';
+                }
+                if (itHasHappenedInTable) {
+                    table.style.overflow = 'hidden';
                 }
                 if (itHasHappenedInPostForm) {
                     form.classList.add('overflow-hidden');
