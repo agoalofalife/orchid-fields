1ead6d791862fa1f14abbf4eb96b792f32670fc9
jakehuang2232 <83938127+jakehuang2232@users.noreply.github.com>
2026-01-07 22:25:48 +0800

    Change display for first column of the table (#16)
    Co-authored-by: jakehuang <jakehuang@softstargames.com.tw>

--- resources/views/partials/layouts/td.blade.php
+++ resources/views/partials/layouts/td.blade.php
@@ -1,17 +1,23 @@
 <td class="text-{{$align}} @if(!$width) text-truncate @endif"
-{{--    data-column="{{ $slug }}"--}}
+    {{--    data-column="{{ $slug }}"--}}
     colspan="{{ $colspan }}"
-        @style([
-        "min-width:$width;" => $width,
-        "$style" => $style,
-         "display:flex" => $isFirst
-        ])
+    @style([
+    "min-width:$width;" => $width,
+    "$style" => $style,
+    ])
 >
-    <div style="padding-left: 2%">
-        @isset($render)
-            {!! $value !!}
-        @else
-            {{ $value }}
-        @endisset
-    </div>
+    @if($isFirst)
+        <div style="display: flex;">
+            @endif
+            <div style="padding-left: 2%;"
+            >
+                @isset($render)
+                    {!! $value !!}
+                @else
+                    {{ $value }}
+                @endisset
+            </div>
+            @if($isFirst)
+        </div>
+    @endif
 </td>
